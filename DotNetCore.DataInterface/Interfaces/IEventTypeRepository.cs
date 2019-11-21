99c8d1034604d53c393cdd348f84536f93f86dd5
royviggo <royviggo@gmail.com>
2019-11-21 20:43:44 +0100

    Delete moved files

--- DotNetCore.DataInterface/Interfaces/IEventTypeRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using DotNetCore.Domain.Entities;
-using System;
-
-namespace DotNetCore.Data
-{
-    public interface IEventTypeRepository : IGenericRepository<EventType>, IDisposable
-    {
-    }
-}
--- DotNetCore.Domain/Entities/Place.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DotNetCore.Domain.Entities
-{
-    public class Place : Entity
-    {
-        public string Name { get; set; }
-    }
-}
